a068a7bbb0d32813679fe21b4bfc7acfc67bf40a
danilomacb <danilo.bakun@gmail.com>
2019-10-04 16:31:23 -0300

    changing config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,16 @@ module.exports = {
   // Customize your site metadata:
   siteMetadata: {
     title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    author: `Danilo Macedo Bakun`,
+    description: `Descrição...`,
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/danilomacb`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/danilomacb`,
       },
     ],
   },
